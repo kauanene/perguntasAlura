e9a723e12c6d1768f29d73cb19cc2180d4389d58
kauanene <kauanevictoria006@gmail.com>
2024-11-21 00:31:00 +0000

    trabalhoalura

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia',
+    'O que é a Microsoft?',
+    'É uma empresa multinacional de tecnologia americana que desenvolve, fabrica, licencia, suporta e vende softwares, dispositivos eletrônicos, hardware e serviços online.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'A capital da Argentina é Buenos Aires'
+)
+
+criaCartao(
+    'Programação',
+    'Pra que serve o CSS?',
+    '. É uma linguagem de estilo que permite definir a apresentação visual de documentos HTML ou XML.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Ponte em Inglês?',
+    'Mais em ingles é Bridge '
+)
